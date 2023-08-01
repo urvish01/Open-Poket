67020ee96946b55f29eb0bed7c1c4d1078dceee1
Urvish Rupareliya <urvishrupareliya@gmail.com>
2023-08-01 16:11:29 +0530

    fix bug

--- task_3.js
+++ task_3.js
@@ -14,7 +14,6 @@ function countWords(filename) {
   fs.readFile(filename, 'utf8', (err, data) => {
     if (err) {
       console.error('Error reading file:', err.message);
-      return;
     }
 
     //count the words
